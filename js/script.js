7818c721a94e80fcd6d404bfc6c3e7f5a2cb7be7
Yuichi Kuwhara <yuichi.kuwahara@monocrea.jp>
2016-12-11 18:24:57 +0900

    sit-wtのページを追加

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 $(function () {
     
     var navbarCollapseLink = $("#navbar-collapse-link");
-    navbarCollapseLink.find("a").click(function () {
+    navbarCollapseLink.find("a[class!='dropdown-toggle']").click(function () {
         navbarCollapseLink.collapse("hide");
     });
     
 
     
     var HEADER_HEIGHT = $("nav").height();
-    $("a[href*=#]").click(function () {
+    $("a[href*='#'][class!='dropdown-toggle']").click(function () {
         var target = $(this.hash);
         if (target) {
             var targetOffset = target.offset().top - HEADER_HEIGHT;
@@ -18,18 +18,20 @@ $(function () {
     });
 });
 
-var swiper = new Swiper('.swiper-container', {
-    pagination: '.swiper-pagination',
-    paginationClickable: true,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    touchRatio: 0.3,
-    parallax: true,
-    calculateHeight: true,
-    autoResize: true,
-    resizeReInit: true,
-    speed: 700,
-    loop: true,
-    autoplay: 15000,
-    spaceBetween : 50
-});
+if (typeof Swiper !== "undefined") {
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        touchRatio: 0.3,
+        parallax: true,
+        calculateHeight: true,
+        autoResize: true,
+        resizeReInit: true,
+        speed: 700,
+        loop: true,
+        autoplay: 15000,
+        spaceBetween : 50
+    });
+}
